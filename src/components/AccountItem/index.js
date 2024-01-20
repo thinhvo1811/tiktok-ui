5c1b78ea1521c365aec4f87ea9feca2e27411dfa
thinhvo18 <thinhvq18112002@gmail.com>
2024-01-20 09:26:13 +0700

    Xây dựng Logic phần Header #1

--- src/components/AccountItem/index.js
+++ src/components/AccountItem/index.js
@@ -1,6 +1,7 @@
 import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import classNames from 'classnames/bind';
+import Image from '~/components/Image';
 import styles from './AccountItem.module.scss';
 
 const cx = classNames.bind(styles);
@@ -8,7 +9,7 @@ const cx = classNames.bind(styles);
 function AccountItem() {
     return (
         <div className={cx('wrapper')}>
-            <img
+            <Image
                 className={cx('avatar')}
                 src="https://scontent.fsgn5-2.fna.fbcdn.net/v/t1.6435-9/117354824_792500984821445_4185575291286150690_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=be3454&_nc_ohc=1DZySV3dMUcAX8LSBdI&_nc_oc=AQkOfBKuHop9aainGQbTUCg3wTtSzYfBoiVSsIEOaIN9z0-oP_5Bnk5ihVVR0suG4l0&_nc_ht=scontent.fsgn5-2.fna&oh=00_AfBkK-Qp2WmCpq1JYEM1z3sE0ifWug5eQlM5R4lcejSrkA&oe=65CC5BD3"
                 alt="Hoaa"
