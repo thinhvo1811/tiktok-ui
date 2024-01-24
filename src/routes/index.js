ca56476ab3bb4ae94e78d9b99fbc0a69e2ab827d
thinhvo18 <thinhvq18112002@gmail.com>
2024-01-24 10:29:31 +0700

    Sửa lỗi và hoàn thiện phần Header #1

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,3 +1,4 @@
+import routesConfig from '~/config/routes';
 import Home from '~/pages/Home';
 import Following from '~/pages/Following';
 import Upload from '~/pages/Upload';
@@ -5,10 +6,10 @@ import Profile from '~/pages/Profile';
 import { HeaderOnly } from '~/components/Layout';
 
 const publicRoutes = [
-    { path: '/', component: Home },
-    { path: '/following', component: Following },
-    { path: '/:nickname', component: Profile },
-    { path: '/upload', component: Upload, layout: HeaderOnly },
+    { path: routesConfig.home, component: Home },
+    { path: routesConfig.following, component: Following },
+    { path: routesConfig.profile, component: Profile },
+    { path: routesConfig.upload, component: Upload, layout: HeaderOnly },
 ];
 
 const privateRoutes = [];
