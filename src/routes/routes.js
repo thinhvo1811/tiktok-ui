a1623ead8fbbe6efe4b02ca595084941c84ce482
thinhvo18 <thinhvq18112002@gmail.com>
2024-02-05 15:40:08 +0700

    Xây dựng UI phần Header #1

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,11 +3,13 @@ import Home from '~/pages/Home';
 import Following from '~/pages/Following';
 import Upload from '~/pages/Upload';
 import Profile from '~/pages/Profile';
+import Live from '~/pages/Live';
 import { HeaderOnly } from '~/layouts';
 
 const publicRoutes = [
     { path: config.routes.home, component: Home },
     { path: config.routes.following, component: Following },
+    { path: config.routes.live, component: Live },
     { path: config.routes.profile, component: Profile },
     { path: config.routes.upload, component: Upload, layout: HeaderOnly },
 ];
